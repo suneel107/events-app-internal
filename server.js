33a5d141f8e3eff4beb854379596eb0df7c70804
suneel107 <suneelchintam@gmail.com>
2022-03-03 05:03:31 +0000

    My first change

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use(function (req, res, next) {
 // from a cloud data store
 const mockEvents = {
     events: [
-        { title: 'an event', id: 1, description: 'something really cool' },
-        { title: 'another event', id: 2, description: 'something even cooler' }
+        { title: 'First event', id: 1, description: 'something really cool' },
+        { title: 'Second event', id: 2, description: 'something even cooler' } // My first change
     ]
 };
 
