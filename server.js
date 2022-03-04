287fb8d6bcbfa5369c78b3c3b171dec8b75f2022
sharvil-majmudar <sharvil.majmudar7@gmail.com>
2022-03-04 05:46:43 +0000

    Changed internal server js

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const bodyParser = require('body-parser');
 // create the server
 const app = express();
 
+const Firestore = require("@google-cloud/firestore");
+
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
+
+
 // the backend server will parse json, not a form request
 app.use(bodyParser.json());
 
@@ -44,8 +53,9 @@ app.get('/version', (req, res) => {
 // mock events endpoint. this would be replaced by a call to a datastore
 // if you went on to develop this as a real application.
 app.get('/events', (req, res) => {
-    res.json(mockEvents);
+    getEvents(req, res);
 });
+
 
 // Adds an event - in a real solution, this would insert into a cloud datastore.
 // Currently this simply adds an event to the mock array in memory
@@ -57,11 +67,13 @@ app.post('/event', (req, res) => {
         description: req.body.description,
         id : mockEvents.events.length + 1
      }
-    // add to the mock array
-    mockEvents.events.push(ev);
-    // return the complete array
-    res.json(mockEvents);
+// this will create the Events collection if it does not exist
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+
 });
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
@@ -76,3 +88,23 @@ const server = app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+function getEvents(req, res) {
+    firestore.collection("Events").get()
+        .then((snapshot) => {
+            if (!snapshot.empty) {
+                const ret = { events: []};
+                snapshot.docs.forEach(element => {
+                    ret.events.push(element.data());
+                }, this);
+                console.log(ret);
+                res.json(ret);
+            } else {
+                 res.json(mockEvents);
+            }
+        })
+        .catch((err) => {
+            console.error('Error getting events', err);
+            res.json(mockEvents);
+        });
+};
